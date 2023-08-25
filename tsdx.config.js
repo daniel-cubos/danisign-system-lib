2bed9a11b8988d159773ac6a49ccc7a291a83f6d
Daniel <danieldeandradelopes@gmail.com>
2023-08-25 14:31:29 -0300

    chore: configure svgr and url for svg files and remove unnecessary items for scss

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,18 +1,14 @@
 const postcss = require('rollup-plugin-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const svgr = require('@svgr/rollup');
+const url = require('@rollup/plugin-url');
 
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
+      svgr(),
+      url(),
       postcss({
         modules: true,
-        plugins: [
-          autoprefixer(),
-          cssnano({
-            preset: 'default',
-          }),
-        ],
         inject: true,
         extract: !!options.writeMeta,
       })
