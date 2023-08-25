87bfd05d7880c843a4f030d0a072f74b368bd732
Daniel <danieldeandradelopes@gmail.com>
2023-08-25 14:30:25 -0300

    feat: create types for svg and png images

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,13 @@ declare module '*.scss' {
   const url: any;
   export default url;
 }
+
+declare module '*.svg' {
+  const url: any;
+  export default url;
+}
+
+declare module '*.png' {
+  const url: any;
+  export default url;
+}
